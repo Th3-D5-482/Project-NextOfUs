3993dee5207fdb95c8fda2ba2e6b59274d80e2c6
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-12-20 23:49:41 +0530

    CodeAdd: Added the starting code

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black border-red-800">
+      <Text className="text-white text-3xl font-bold">Hello World!</Text>
+    </View>
+  );
+}

f972b248c9ba43af96f7df159b8a6187b2ad1996
Th3-D5-482 <123230201+Th3-D5-482@users.noreply.github.com>
2025-12-23 23:10:35 +0530

    Routing and Navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,54 @@
-import { Ionicons } from '@expo/vector-icons';
+import { icons } from '@/constants/icons';
+import { images } from '@/constants/images';
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { Image, ImageBackground, Text, View } from 'react-native';
+
+const TabIcon = ({focused, icons, title}: any) => {
+  if(focused){
+    return (
+    <ImageBackground source={images.highlight} className='flex-row w-[90px] justify-center items-center rounded-full overflow-hidden mt-3' style={{height: 52}}  > 
+            <Image source={icons} tintColor="#151312" className='size-5'/>
+            <Text className='text-secondary text-base font-semibold ml-2'>{title}</Text>
+            </ImageBackground>
+  )
+  }
+  else {
+    return (
+    <View className='size-full justify-center items-center mt-4 rounded-full'>
+      <Image source={icons}  tintColor="#A8B5DB " className='w-5'/>
+    </View>
+    )
+  }
+}
 
 const _layout = () => {
   return (
-    <Tabs screenOptions={{ tabBarStyle: { backgroundColor: '#121212' } }}>
-        <Tabs.Screen name="index" options={{ headerShown: false, tabBarLabel: 'Home',tabBarIcon: ({ color, size }) => (
-        <Ionicons name="home" color={color} size={size} />
-      ), }} />
-        <Tabs.Screen name="search" options={{ headerShown: false, tabBarLabel: 'Search' }} />
-        <Tabs.Screen name="profile" options={{ headerShown: false, tabBarLabel: 'Profile' }} />
+    <Tabs screenOptions={{tabBarShowLabel: false,tabBarItemStyle: {
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    tabBarStyle: {
+      backgroundColor: '#0F0D23',
+      borderRadius: 50,
+      marginHorizontal: 20,
+      marginBottom: 36,
+      height: 52,
+      position: 'absolute',
+      overflow: 'hidden',
+      borderWidth: 1,
+      borderColor: '#0F0D23',
+    }
+  }
+    }>
+      <Tabs.Screen name="index" options={{ headerShown: false, title: 'Home', tabBarIcon: ({focused}) => 
+       <TabIcon focused = {focused} icons ={icons.home} title ="Home" />
+      }} />
+        <Tabs.Screen name="search" options={{ headerShown: false, title: 'Search', tabBarIcon: ({focused}) => <TabIcon focused = {focused} icons ={icons.search} title ="Search" /> }} />
+      <Tabs.Screen name ="saved" options={{headerShown: false, title: "Saved", tabBarIcon: ({focused}) => <TabIcon focused = {focused} icons ={icons.save} title ="Saved" />}}/>
+      <Tabs.Screen name ="profile" options={{headerShown: false, title: "Profile", tabBarIcon: ({focused}) => <TabIcon focused = {focused} icons ={icons.person} title ="Profile" />}}/>
     </Tabs>
   )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 const index = () => {
   return (
-    <View className='flex-1 justify-center items-center bg-black'>
-      <Text className='text-accent text-4xl font-bold'>Welcome</Text>
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-4xl font-bold'>Welcome!</Text>
     </View>
   )
 }
